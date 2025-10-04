7351605467f69dccd1eb655a98495dc9676e6b91
yaito3014 <ykakeyama3014@gmail.com>
2025-10-04 18:26:04 +0900

    Consider P2587R3

--- test/cxx11/apply.cpp
+++ test/cxx11/apply.cpp
@@ -6,6 +6,7 @@
 #include <string>
 #include <tuple>
 #include <utility>
+#include <version>
 
 #include <cstddef>
 
@@ -15,10 +16,18 @@ TEST_CASE("apply")
 {
   {
     auto func = [](int i, double d) -> std::string { return std::to_string(i) + "_" + std::to_string(d); };
+#if __cpp_lib_to_string >= 202306L
+    CHECK(pf::apply(func, std::make_pair(42, 3.14)) == "42_3.14");
+#else
     CHECK(pf::apply(func, std::make_pair(42, 3.14)) == "42_3.140000");
+#endif
   }
   {
     auto func = [](int i, double d, std::string s) -> std::string { return std::to_string(i) + "_" + std::to_string(d) + "_" + s; };
+#if __cpp_lib_to_string >= 202306L
+    CHECK(pf::apply(func, std::make_tuple(42, 3.14, "foo")) == "42_3.14_foo");
+#else
     CHECK(pf::apply(func, std::make_tuple(42, 3.14, "foo")) == "42_3.140000_foo");
+#endif
   }
 }
