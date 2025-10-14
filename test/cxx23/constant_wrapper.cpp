a4ba18a5f9ac218985557c799c2beb35ad96f49a
yaito3014 <ykakeyama3014@gmail.com>
2025-10-14 22:40:15 +0900

    Fix typo

--- test/cxx23/constant_wrapper.cpp
+++ test/cxx23/constant_wrapper.cpp
@@ -15,7 +15,7 @@ TEST_CASE("constant_wrapper")
   STATIC_REQUIRE(pf::cw<12> % pf::cw<34> == pf::cw<12>);
 
   STATIC_REQUIRE(++pf::cw<12> == pf::cw<13>);
-  STATIC_REQUIRE(pf::cw<std::array{33, 4}>[pf::cw<0>] = pf::cw<33>);
+  STATIC_REQUIRE(pf::cw<std::array{33, 4}>[pf::cw<0>] == pf::cw<33>);
 
   // TODO: more tests
 }
