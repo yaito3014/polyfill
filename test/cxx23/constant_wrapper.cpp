46cc5749defc9267863be4ee4f988a84ead8d31a
yaito3014 <ykakeyama3014@gmail.com>
2025-10-14 22:52:58 +0900

    Add multidimentional test

--- test/cxx23/constant_wrapper.cpp
+++ test/cxx23/constant_wrapper.cpp
@@ -6,6 +6,14 @@
 
 namespace pf = yk::polyfill;
 
+#if __cpp_multidimensional_subscript >= 202211L
+
+struct SubscriptIsAdd {
+  constexpr auto operator[](auto x, auto y) const { return x + y; }
+};
+
+#endif
+
 TEST_CASE("constant_wrapper")
 {
   STATIC_REQUIRE(pf::cw<12> + pf::cw<34> == pf::cw<46>);
@@ -16,6 +24,10 @@ TEST_CASE("constant_wrapper")
 
   STATIC_REQUIRE(++pf::cw<12> == pf::cw<13>);
   STATIC_REQUIRE(pf::cw<std::array{33, 4}>[pf::cw<0>] == pf::cw<33>);
+
+#if __cpp_multidimensional_subscript >= 202211L
+  STATIC_REQUIRE(pf::cw<SubscriptIsAdd{}>[pf::cw<33>, pf::cw<4>] == pf::cw<37>);
+#endif
 
   // TODO: more tests
 }
