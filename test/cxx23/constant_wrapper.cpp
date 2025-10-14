368addc86dc3a223586f56595422ddd21e29ad04
yaito3014 <ykakeyama3014@gmail.com>
2025-10-14 23:58:59 +0900

    Guard test using explicit this parameter

--- test/cxx23/constant_wrapper.cpp
+++ test/cxx23/constant_wrapper.cpp
@@ -22,8 +22,10 @@ TEST_CASE("constant_wrapper")
   STATIC_REQUIRE(pf::cw<12> / pf::cw<34> == pf::cw<0>);
   STATIC_REQUIRE(pf::cw<12> % pf::cw<34> == pf::cw<12>);
 
+#if __cpp_explicit_this_parameter >= 202110L
   STATIC_REQUIRE(++pf::cw<12> == pf::cw<13>);
   STATIC_REQUIRE(pf::cw<std::array{33, 4}>[pf::cw<0>] == pf::cw<33>);
+#endif
 
 #if __cpp_multidimensional_subscript >= 202211L
   STATIC_REQUIRE(pf::cw<SubscriptIsAdd{}>[pf::cw<33>, pf::cw<4>] == pf::cw<37>);
