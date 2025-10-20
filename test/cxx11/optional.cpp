6e3042bf2bedf6648271642e857daca1c6182a97
yaito3014 <ykakeyama3014@gmail.com>
2025-10-20 20:42:13 +0900

    Add test

--- test/cxx11/optional.cpp
+++ test/cxx11/optional.cpp
@@ -19,6 +19,14 @@ TEST_CASE("optional")
   STATIC_REQUIRE(std::is_trivially_move_assignable<pf::optional<int>>::value);
   STATIC_REQUIRE(std::is_trivially_destructible<pf::optional<int>>::value);
 
-  pf::optional<int> opt;
-  CHECK(!opt.has_value());
+  {
+    pf::optional<int> opt;
+    CHECK(!opt.has_value());
+  }
+
+  {
+    pf::optional<int> opt(pf::in_place_holder::value, 42);
+    CHECK(opt.has_value());
+    CHECK(*opt == 42);
+  }
 }
