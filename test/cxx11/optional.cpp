a1d2232672672d0095a4ee3aa91a59aad7dc70da
yaito3014 <ykakeyama3014@gmail.com>
2025-10-21 14:52:51 +0900

    Add generic copy/move construction

--- test/cxx11/optional.cpp
+++ test/cxx11/optional.cpp
@@ -7,8 +7,14 @@
 #include <yk/polyfill/optional.hpp>
 
 #include <type_traits>
+#include <utility>
 
 namespace pf = yk::polyfill;
+
+struct NeedExplicitConvertion {
+  int value;
+  explicit NeedExplicitConvertion(int arg) : value(arg) {}
+};
 
 TEST_CASE("optional")
 {
@@ -19,21 +25,71 @@ TEST_CASE("optional")
   STATIC_REQUIRE(std::is_trivially_move_assignable<pf::optional<int>>::value);
   STATIC_REQUIRE(std::is_trivially_destructible<pf::optional<int>>::value);
 
+  // default construction
   {
     pf::optional<int> opt;
     CHECK(!opt.has_value());
   }
 
+  // in-place construction
   {
     pf::optional<int> opt(pf::in_place_holder::value, 42);
     CHECK(opt.has_value());
     CHECK(*opt == 42);
   }
 
+  // generic construction
   {
     pf::optional<int> opt = 42;
     CHECK(opt.has_value());
     CHECK(*opt == 42);
+  }
+
+  // generic copy construction
+  {
+    pf::optional<int> a = 42;
+    pf::optional<double> b = a;
+    CHECK(*b == 42.0);
+  }
+
+  // generic move construction
+  {
+    pf::optional<int> a = 42;
+    pf::optional<double> b = std::move(a);
+    CHECK(*b == 42.0);
+  }
+
+  // boolean tolerance test
+  {
+    {
+      pf::optional<int> a;
+      pf::optional<bool> b = a;
+      CHECK(!a.has_value());
+      CHECK(!b.has_value());
+    }
+
+    {
+      pf::optional<int> a = 42;
+      pf::optional<bool> b = a;
+      CHECK(b.has_value());
+      CHECK(*b);
+    }
+
+    {
+      pf::optional<int> a = 0;
+      pf::optional<bool> b = a;
+      CHECK(b.has_value());
+      CHECK(!*b);
+    }
+  }
+
+  // explicitation check
+  {
+    STATIC_REQUIRE(std::is_constructible<pf::optional<NeedExplicitConvertion>, int>::value);
+    STATIC_REQUIRE(!std::is_convertible<int, pf::optional<NeedExplicitConvertion>>::value);
+
+    STATIC_REQUIRE(std::is_constructible<pf::optional<NeedExplicitConvertion>, pf::optional<int>>::value);
+    STATIC_REQUIRE(!std::is_convertible<pf::optional<int>, pf::optional<NeedExplicitConvertion>>::value);
   }
 
   // TODO: add non trivial tests
