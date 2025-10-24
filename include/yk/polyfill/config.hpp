2a82f256bc53b872c4e6a1320e2731477c6d14a7
yaito3014 <ykakeyama3014@gmail.com>
2025-10-24 10:52:37 +0900

    Adjust constexpr

--- include/yk/polyfill/config.hpp
+++ include/yk/polyfill/config.hpp
@@ -7,6 +7,12 @@
 #define YK_POLYFILL_CXX14_CONSTEXPR
 #endif
 
+#if __cplusplus >= 201703L
+#define YK_POLYFILL_CXX17_CONSTEXPR constexpr
+#else
+#define YK_POLYFILL_CXX17_CONSTEXPR
+#endif
+
 #if __cplusplus >= 202002L
 #define YK_POLYFILL_CXX20_CONSTEXPR constexpr
 #else
