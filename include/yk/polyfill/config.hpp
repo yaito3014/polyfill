7a1b55c683927ca94d2fe2b41e58456bd786e6a3
yaito3014 <ykakeyama3014@gmail.com>
2025-11-04 16:36:36 +0900

    Add constexpr macros

--- include/yk/polyfill/config.hpp
+++ include/yk/polyfill/config.hpp
@@ -7,4 +7,22 @@
 #define YK_POLYFILL_CXX14_CONSTEXPR
 #endif
 
+#if __cplusplus >= 201703L
+#define YK_POLYFILL_CXX17_CONSTEXPR constexpr
+#else
+#define YK_POLYFILL_CXX17_CONSTEXPR
+#endif
+
+#if __cplusplus >= 202002L
+#define YK_POLYFILL_CXX20_CONSTEXPR constexpr
+#else
+#define YK_POLYFILL_CXX20_CONSTEXPR
+#endif
+
+#if __cplusplus >= 202302L
+#define YK_POLYFILL_CXX23_CONSTEXPR constexpr
+#else
+#define YK_POLYFILL_CXX23_CONSTEXPR
+#endif
+
 #endif  // YK_POLYFILL_CONFIG_HPP
