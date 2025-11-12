92a8000dde8e19083c3a23ff38932865c715211b
yaito3014 <ykakeyama3014@gmail.com>
2025-11-12 15:58:01 +0900

    Add exposition-only helper

--- include/yk/polyfill/bits/optional_common.hpp
+++ include/yk/polyfill/bits/optional_common.hpp
@@ -1,7 +1,7 @@
 #ifndef YK_POLYFILL_BITS_OPTIONAL_COMMON_HPP
 #define YK_POLYFILL_BITS_OPTIONAL_COMMON_HPP
 
-#include <yk/polyfill/config.hpp>
+#include <yk/polyfill/type_traits.hpp>
 
 #include <exception>
 
@@ -30,6 +30,15 @@ class bad_optional_access : public std::exception {
 public:
   char const* what() const noexcept override { return "accessing empty optional"; }
 };
+
+namespace optional_detail {
+
+template<class T, class U>
+constexpr bool converts_from_any_cvref = disjunction<
+    std::is_constructible<T, U&>, std::is_convertible<U&, T>, std::is_constructible<T, U>, std::is_convertible<U, T>, std::is_constructible<T, U const&>,
+    std::is_convertible<U const&, T>, std::is_constructible<T, U const>, std::is_convertible<U const, T>>::value;
+
+}  // namespace optional_detail
 
 }  // namespace polyfill
 
