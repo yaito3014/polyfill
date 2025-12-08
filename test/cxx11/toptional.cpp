75084bd18733b7776c323519783bec6e24e59f94
yaito3014 <ykakeyama3014@gmail.com>
2025-12-08 17:12:47 +0900

    Add non-trivial test case

--- test/cxx11/toptional.cpp
+++ test/cxx11/toptional.cpp
@@ -6,7 +6,8 @@
 
 #include <yk/polyfill/extension/toptional.hpp>
 
-namespace ext = yk::polyfill::extension;
+namespace pf = yk::polyfill;
+namespace ext = pf::extension;
 
 // Custom trait that considers -1 as tombstone
 template<class T>
@@ -304,7 +305,7 @@ TEST_CASE("toptional - transform with custom traits")
   SECTION("transform with custom traits")
   {
     ext::toptional<int> opt = 5;
-    auto result = opt.transform<minus_one_traits>([](int x) { return x * 2; });
+    auto result = opt.transform<minus_one_traits<int>>([](int x) { return x * 2; });
     CHECK(result.has_value());
     CHECK(*result == 10);
 
@@ -330,12 +331,291 @@ TEST_CASE("toptional - transform with custom traits")
     CHECK(*step1 == 10);
 
     // Second transform with custom traits (-1 is tombstone)
-    auto step2 = step1.transform<minus_one_traits>([](int x) { return x - 5; });  // 5
+    auto step2 = step1.transform<minus_one_traits<int>>([](int x) { return x - 5; });  // 5
     CHECK(step2.has_value());
     CHECK(*step2 == 5);
 
     // Third transform back to default traits
     // This should throw because the result (0) is the tombstone value for non_zero_traits
     CHECK_THROWS_AS(step2.transform([](int x) { return x - 5; }), ext::bad_toptional_initialization);
+  }
+}
+
+// Non-trivial test types
+namespace {
+
+struct LifetimeTracker {
+  static int constructions;
+  static int destructions;
+  static int copies;
+  static int moves;
+
+  static void reset() { constructions = destructions = copies = moves = 0; }
+
+  int value;
+
+  explicit LifetimeTracker(int v = 1) : value(v) { ++constructions; }
+  ~LifetimeTracker() { ++destructions; }
+
+  LifetimeTracker(LifetimeTracker const& other) : value(other.value)
+  {
+    ++constructions;
+    ++copies;
+  }
+
+  LifetimeTracker(LifetimeTracker&& other) noexcept : value(other.value)
+  {
+    ++constructions;
+    ++moves;
+  }
+
+  LifetimeTracker& operator=(LifetimeTracker const& other)
+  {
+    value = other.value;
+    ++copies;
+    return *this;
+  }
+
+  LifetimeTracker& operator=(LifetimeTracker&& other) noexcept
+  {
+    value = other.value;
+    ++moves;
+    return *this;
+  }
+};
+
+int LifetimeTracker::constructions = 0;
+int LifetimeTracker::destructions = 0;
+int LifetimeTracker::copies = 0;
+int LifetimeTracker::moves = 0;
+
+// Custom traits for LifetimeTracker (value 0 is tombstone)
+struct lifetime_tracker_traits {
+  static bool is_engaged(LifetimeTracker const& x) noexcept { return x.value != 0; }
+  static LifetimeTracker tombstone_value() noexcept { return LifetimeTracker{0}; }
+};
+
+struct NonTrivialWithDestructor {
+  static int destructor_calls;
+  int value;
+
+  explicit NonTrivialWithDestructor(int v) : value(v) {}
+
+  ~NonTrivialWithDestructor() { ++destructor_calls; }
+
+  NonTrivialWithDestructor(NonTrivialWithDestructor const& other) : value(other.value) {}
+
+  NonTrivialWithDestructor(NonTrivialWithDestructor&& other) noexcept : value(other.value) {}
+
+  NonTrivialWithDestructor& operator=(NonTrivialWithDestructor const& other)
+  {
+    value = other.value;
+    return *this;
+  }
+
+  NonTrivialWithDestructor& operator=(NonTrivialWithDestructor&& other) noexcept
+  {
+    value = other.value;
+    return *this;
+  }
+};
+
+int NonTrivialWithDestructor::destructor_calls = 0;
+
+// Traits for NonTrivialWithDestructor (value -1 is tombstone)
+struct non_trivial_traits {
+  static bool is_engaged(NonTrivialWithDestructor const& x) noexcept { return x.value != -1; }
+  static NonTrivialWithDestructor tombstone_value() noexcept { return NonTrivialWithDestructor{-1}; }
+};
+}  // namespace
+
+TEST_CASE("toptional - non-trivial types with lifetime tracking")
+{
+  SECTION("basic construction and destruction")
+  {
+    LifetimeTracker::reset();
+
+    {
+      ext::toptional<LifetimeTracker, lifetime_tracker_traits> opt{pf::in_place_holder::value, 42};
+      CHECK(opt.has_value());
+      CHECK(opt->value == 42);
+      CHECK(LifetimeTracker::constructions >= 1);
+    }
+
+    // Destructor should be called when opt goes out of scope
+    CHECK(LifetimeTracker::destructions >= 1);
+  }
+
+  SECTION("disengaged state doesn't call destructor on tombstone")
+  {
+    LifetimeTracker::reset();
+
+    {
+      ext::toptional<LifetimeTracker, lifetime_tracker_traits> opt;
+      CHECK(!opt.has_value());
+    }
+
+    // Destructor is still called for the tombstone value in storage
+    CHECK(LifetimeTracker::destructions > 0);
+  }
+
+  SECTION("copy construction")
+  {
+    LifetimeTracker::reset();
+
+    ext::toptional<LifetimeTracker, lifetime_tracker_traits> opt1{pf::in_place_holder::value, 42};
+    int copies_before = LifetimeTracker::copies;
+
+    ext::toptional<LifetimeTracker, lifetime_tracker_traits> opt2 = opt1;
+
+    CHECK(opt2.has_value());
+    CHECK(opt2->value == 42);
+    CHECK(LifetimeTracker::copies > copies_before);
+  }
+
+  SECTION("move construction")
+  {
+    LifetimeTracker::reset();
+
+    ext::toptional<LifetimeTracker, lifetime_tracker_traits> opt1{pf::in_place_holder::value, 42};
+    int moves_before = LifetimeTracker::moves;
+
+    ext::toptional<LifetimeTracker, lifetime_tracker_traits> opt2 = std::move(opt1);
+
+    CHECK(opt2.has_value());
+    CHECK(opt2->value == 42);
+    CHECK(LifetimeTracker::moves > moves_before);
+  }
+
+  SECTION("copy assignment")
+  {
+    LifetimeTracker::reset();
+
+    ext::toptional<LifetimeTracker, lifetime_tracker_traits> opt1{pf::in_place_holder::value, 42};
+    ext::toptional<LifetimeTracker, lifetime_tracker_traits> opt2{pf::in_place_holder::value, 99};
+
+    int copies_before = LifetimeTracker::copies;
+    opt2 = opt1;
+
+    CHECK(opt2.has_value());
+    CHECK(opt2->value == 42);
+    CHECK(LifetimeTracker::copies > copies_before);
+  }
+
+  SECTION("move assignment")
+  {
+    LifetimeTracker::reset();
+
+    ext::toptional<LifetimeTracker, lifetime_tracker_traits> opt1{pf::in_place_holder::value, 42};
+    ext::toptional<LifetimeTracker, lifetime_tracker_traits> opt2{pf::in_place_holder::value, 99};
+
+    int moves_before = LifetimeTracker::moves;
+    opt2 = std::move(opt1);
+
+    CHECK(opt2.has_value());
+    CHECK(opt2->value == 42);
+    CHECK(LifetimeTracker::moves > moves_before);
+  }
+
+  SECTION("emplace")
+  {
+    LifetimeTracker::reset();
+
+    ext::toptional<LifetimeTracker, lifetime_tracker_traits> opt{pf::in_place_holder::value, 42};
+    CHECK(opt->value == 42);
+
+    int destructions_before = LifetimeTracker::destructions;
+    opt.emplace(99);
+
+    CHECK(opt.has_value());
+    CHECK(opt->value == 99);
+    // Old value should be destroyed
+    CHECK(LifetimeTracker::destructions > destructions_before);
+  }
+
+  SECTION("reset")
+  {
+    LifetimeTracker::reset();
+
+    ext::toptional<LifetimeTracker, lifetime_tracker_traits> opt{pf::in_place_holder::value, 42};
+    CHECK(opt.has_value());
+
+    int destructions_before = LifetimeTracker::destructions;
+    opt.reset();
+
+    CHECK(!opt.has_value());
+    // Old value should be destroyed
+    CHECK(LifetimeTracker::destructions > destructions_before);
+  }
+
+  SECTION("monadic operations preserve lifetime semantics")
+  {
+    LifetimeTracker::reset();
+
+    ext::toptional<LifetimeTracker, lifetime_tracker_traits> opt{pf::in_place_holder::value, 5};
+
+    auto result = opt.transform<lifetime_tracker_traits>([](LifetimeTracker const& lt) { return LifetimeTracker{lt.value * 2}; });
+
+    CHECK(result.has_value());
+    CHECK(result->value == 10);
+    CHECK(LifetimeTracker::constructions > 0);
+  }
+}
+
+TEST_CASE("toptional - non-trivial type with destructor side effects")
+{
+  SECTION("destructor called on emplace")
+  {
+    NonTrivialWithDestructor::destructor_calls = 0;
+
+    ext::toptional<NonTrivialWithDestructor, non_trivial_traits> opt{pf::in_place_holder::value, 42};
+    CHECK(opt.has_value());
+    CHECK(opt->value == 42);
+
+    int calls_before = NonTrivialWithDestructor::destructor_calls;
+    opt.emplace(99);
+
+    CHECK(opt.has_value());
+    CHECK(opt->value == 99);
+    // Old value's destructor should have been called
+    CHECK(NonTrivialWithDestructor::destructor_calls > calls_before);
+  }
+
+  SECTION("destructor called on reset")
+  {
+    NonTrivialWithDestructor::destructor_calls = 0;
+
+    ext::toptional<NonTrivialWithDestructor, non_trivial_traits> opt{pf::in_place_holder::value, 42};
+    CHECK(opt.has_value());
+
+    int calls_before = NonTrivialWithDestructor::destructor_calls;
+    opt.reset();
+
+    CHECK(!opt.has_value());
+    CHECK(NonTrivialWithDestructor::destructor_calls > calls_before);
+  }
+
+  SECTION("destructor called when toptional goes out of scope")
+  {
+    NonTrivialWithDestructor::destructor_calls = 0;
+
+    {
+      ext::toptional<NonTrivialWithDestructor, non_trivial_traits> opt{pf::in_place_holder::value, 42};
+      CHECK(opt.has_value());
+    }
+
+    // Destructor should have been called
+    CHECK(NonTrivialWithDestructor::destructor_calls > 0);
+  }
+
+  SECTION("move construction preserves value")
+  {
+    NonTrivialWithDestructor::destructor_calls = 0;
+
+    ext::toptional<NonTrivialWithDestructor, non_trivial_traits> opt1{pf::in_place_holder::value, 42};
+    ext::toptional<NonTrivialWithDestructor, non_trivial_traits> opt2 = std::move(opt1);
+
+    CHECK(opt2.has_value());
+    CHECK(opt2->value == 42);
   }
 }
