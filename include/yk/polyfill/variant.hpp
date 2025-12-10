a64423f879e891dec865e427d2fa47a5d1edbd93
yaito3014 <ykakeyama3014@gmail.com>
2025-12-10 17:13:57 +0900

    Add basic declarations

---
+++ include/yk/polyfill/variant.hpp
@@ -0,0 +1,46 @@
+#ifndef YK_POLYFILL_VARIANT_HPP
+#define YK_POLYFILL_VARIANT_HPP
+
+#include <yk/polyfill/config.hpp>
+
+#include <yk/polyfill/bits/core_traits.hpp>
+#include <yk/polyfill/extension/pack_indexing.hpp>
+
+#include <cstddef>
+
+#if __cplusplus >= 202002L
+#include <compare>
+#endif
+
+namespace yk {
+
+namespace polyfill {
+
+template<class... Ts>
+class variant;
+
+template<class T>
+struct variant_size;
+
+template<class T>
+struct variant_size<T const> : variant_size<T> {};
+
+template<class... Ts>
+struct variant_size<variant<Ts...>> : integral_constant<std::size_t, sizeof...(Ts)> {};
+
+template<std::size_t I, class T>
+struct variant_alternative;
+
+template<std::size_t I, class T>
+struct variant_alternative<I, T const> : variant_alternative<I, T> {};
+
+template<std::size_t I, class... Ts>
+struct variant_alternative<I, variant<Ts...>> : extension::pack_indexing<I, Ts...> {};
+
+YK_POLYFILL_INLINE constexpr std::size_t variant_npos = -1;
+
+}  // namespace polyfill
+
+}  // namespace yk
+
+#endif  // YK_POLYFILL_VARIANT_HPP
