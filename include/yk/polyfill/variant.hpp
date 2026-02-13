65f14eada043b81987691a19893fd567592b28f5
yaito3014 <ykakeyama3014@gmail.com>
2026-02-13 23:55:45 +0900

    Set index after construction

--- include/yk/polyfill/variant.hpp
+++ include/yk/polyfill/variant.hpp
@@ -332,6 +332,7 @@ struct variant_storage {
   )
   {
     polyfill::construct_at(&storage, in_place_index_t<I>{}, std::forward<Args>(args)...);
+    index = I;
   }
 
   YK_POLYFILL_CXX20_CONSTEXPR void _copy_construct(variant_storage const& other) noexcept(conjunction<std::is_nothrow_copy_constructible<Ts>...>::value)
