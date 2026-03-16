cdcc263d24834c31e96aed5825e7ffebcff247c6
yaito3014 <ykakeyama3014@gmail.com>
2026-03-16 12:08:33 +0900

    Refactor `bit_cast`

--- include/yk/polyfill/bit.hpp
+++ include/yk/polyfill/bit.hpp
@@ -18,19 +18,22 @@ namespace yk {
 
 namespace polyfill {
 
+// `dst` second parameter is a scratch space for memcpy destination in the pre-C++20 fallback.
+// Supply an arbitrary `To` value to bypass the default construction requirement for non-default-constructible types.
+// Ignored when std::bit_cast is available.
 template<
     class To, class From,
     typename std::enable_if<
         conjunction<std::is_trivially_copyable<From>, std::is_trivially_copyable<To>, bool_constant<sizeof(From) == sizeof(To)>>::value, std::nullptr_t>::type =
         nullptr>
-YK_POLYFILL_CXX20_CONSTEXPR To bit_cast(From const& from) noexcept
+YK_POLYFILL_CXX20_CONSTEXPR To bit_cast(From const& from, To dst = To{}) noexcept
 {
 #if __cpp_lib_bit_cast >= 201806L
+  (void)dst;
   return std::bit_cast<To>(from);
 #else
-  alignas(To) unsigned char storage[sizeof(To)];
-  std::memcpy(&storage, std::addressof(from), sizeof(From));
-  return *reinterpret_cast<To*>(&storage);
+  std::memcpy(std::addressof(dst), std::addressof(from), sizeof(To));
+  return dst;
 #endif
 }
 
